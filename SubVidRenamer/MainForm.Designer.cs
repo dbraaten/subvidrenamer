09710ca1600d0dc2908f4e152ff7dbe83a91314a
david <dbraaten@gmail.com>
2018-05-08 15:51:40 -0700

    Initial Revision

---
+++ SubVidRenamer/MainForm.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace SubVidRenamer
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textFolder = new System.Windows.Forms.TextBox();
+			this.textExtensions = new System.Windows.Forms.TextBox();
+			this.listBoxFinal = new System.Windows.Forms.ListBox();
+			this.checkSetNames = new System.Windows.Forms.CheckBox();
+			this.listBoxSource = new System.Windows.Forms.ListBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.buttonBrowse = new System.Windows.Forms.Button();
+			this.buttonRename = new System.Windows.Forms.Button();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// textFolder
+			// 
+			this.textFolder.Location = new System.Drawing.Point(147, 35);
+			this.textFolder.Name = "textFolder";
+			this.textFolder.Size = new System.Drawing.Size(278, 20);
+			this.textFolder.TabIndex = 0;
+			this.textFolder.TextChanged += new System.EventHandler(this.OnFolderChanged);
+			// 
+			// textExtensions
+			// 
+			this.textExtensions.Location = new System.Drawing.Point(147, 61);
+			this.textExtensions.Name = "textExtensions";
+			this.textExtensions.Size = new System.Drawing.Size(278, 20);
+			this.textExtensions.TabIndex = 1;
+			this.textExtensions.TextChanged += new System.EventHandler(this.OnExtensionsChanged);
+			// 
+			// listBoxFinal
+			// 
+			this.listBoxFinal.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listBoxFinal.FormattingEnabled = true;
+			this.listBoxFinal.Location = new System.Drawing.Point(447, 3);
+			this.listBoxFinal.Name = "listBoxFinal";
+			this.listBoxFinal.Size = new System.Drawing.Size(438, 388);
+			this.listBoxFinal.TabIndex = 2;
+			// 
+			// checkSetNames
+			// 
+			this.checkSetNames.AutoSize = true;
+			this.checkSetNames.Location = new System.Drawing.Point(449, 61);
+			this.checkSetNames.Name = "checkSetNames";
+			this.checkSetNames.Size = new System.Drawing.Size(229, 17);
+			this.checkSetNames.TabIndex = 3;
+			this.checkSetNames.Text = "Set names to immediate parent folder name";
+			this.checkSetNames.UseVisualStyleBackColor = true;
+			this.checkSetNames.CheckedChanged += new System.EventHandler(this.OnCheckedSetNames);
+			// 
+			// listBoxSource
+			// 
+			this.listBoxSource.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listBoxSource.FormattingEnabled = true;
+			this.listBoxSource.Location = new System.Drawing.Point(3, 3);
+			this.listBoxSource.Name = "listBoxSource";
+			this.listBoxSource.Size = new System.Drawing.Size(438, 388);
+			this.listBoxSource.TabIndex = 4;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(60, 38);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(36, 13);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Folder";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(60, 65);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(58, 13);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Extensions";
+			// 
+			// buttonBrowse
+			// 
+			this.buttonBrowse.Location = new System.Drawing.Point(449, 33);
+			this.buttonBrowse.Name = "buttonBrowse";
+			this.buttonBrowse.Size = new System.Drawing.Size(78, 22);
+			this.buttonBrowse.TabIndex = 7;
+			this.buttonBrowse.Text = "Browse...";
+			this.buttonBrowse.UseVisualStyleBackColor = true;
+			this.buttonBrowse.Click += new System.EventHandler(this.OnClickedBrowse);
+			// 
+			// buttonRename
+			// 
+			this.buttonRename.Location = new System.Drawing.Point(562, 32);
+			this.buttonRename.Name = "buttonRename";
+			this.buttonRename.Size = new System.Drawing.Size(77, 22);
+			this.buttonRename.TabIndex = 8;
+			this.buttonRename.Text = "Rename";
+			this.buttonRename.UseVisualStyleBackColor = true;
+			this.buttonRename.Click += new System.EventHandler(this.OnClickedRename);
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.textFolder);
+			this.splitContainer1.Panel1.Controls.Add(this.buttonRename);
+			this.splitContainer1.Panel1.Controls.Add(this.textExtensions);
+			this.splitContainer1.Panel1.Controls.Add(this.buttonBrowse);
+			this.splitContainer1.Panel1.Controls.Add(this.checkSetNames);
+			this.splitContainer1.Panel1.Controls.Add(this.label2);
+			this.splitContainer1.Panel1.Controls.Add(this.label1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+			this.splitContainer1.Size = new System.Drawing.Size(888, 512);
+			this.splitContainer1.SplitterDistance = 114;
+			this.splitContainer1.TabIndex = 9;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Controls.Add(this.listBoxFinal, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.listBoxSource, 0, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(888, 394);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(888, 512);
+			this.Controls.Add(this.splitContainer1);
+			this.Name = "MainForm";
+			this.Text = "DigiBee Renamer";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OnClosing);
+			this.Load += new System.EventHandler(this.OnLoad);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel1.PerformLayout();
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textFolder;
+		private System.Windows.Forms.TextBox textExtensions;
+		private System.Windows.Forms.ListBox listBoxFinal;
+		private System.Windows.Forms.CheckBox checkSetNames;
+		private System.Windows.Forms.ListBox listBoxSource;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button buttonBrowse;
+		private System.Windows.Forms.Button buttonRename;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+	}
+}
+
---
+++ SubVidRenamer/MainForm.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.IO;
+
+/// <summary>
+/// A simple utility to help with renaming files that exist in folders that have more correct names than
+/// the files themselves.
+/// </summary>
+namespace SubVidRenamer
+{
+	public partial class MainForm : Form
+	{
+		private static string SVRregPath = @"Software\DigiBee\Renamer";
+		private List<string> sourcePaths;
+		private List<string> destPaths;
+		public MainForm()
+		{
+			InitializeComponent();
+			sourcePaths = new List<string>();
+			destPaths = new List<string>();
+		}
+
+		private void OnLoad(object sender, EventArgs e)
+		{
+			RegistryKey rk = null;
+			rk = Registry.CurrentUser;
+			using (RegistryKey ak = rk.OpenSubKey(SVRregPath))
+			{
+				if (ak != null)
+				{
+					int? setName = ak.GetValue("Set") as int?;
+					if (setName != null)
+					{
+						checkSetNames.Checked = setName != 0;
+					}
+
+					string parm = ak.GetValue("LastFolder") as string;
+					if (parm != null)
+					{
+						textFolder.Text = parm;
+					}
+					parm = ak.GetValue("Extensions") as string;
+					if (parm != null)
+					{
+						textExtensions.Text = parm;
+					}
+					else
+					{
+						textExtensions.Text = "mp4;mkv;avi";
+					}
+				}
+			}
+			//Following is not necessary, when textExtensions.Text is set, it will scan from the event handler
+			//if (textFolder.Text.Count() > 0 && textExtensions.Text.Count() > 0)
+			//{
+			//	ScanFolder();
+			//}
+		}
+
+		/// <summary>
+		/// Find files in fp that have one of the extensions in exts and add them to the sourcePaths
+		/// </summary>
+		/// <param name="fp">The folder path</param>
+		/// <param name="exts">The extensions</param>
+		private void ScanForFiles(string fp, string[] exts)
+		{
+			foreach (string ext in exts)
+			{
+				IEnumerable<string> filelist = null;
+				try
+				{
+					string fileglob = string.Format("*.{0}", ext);
+					filelist = Directory.EnumerateFiles(fp, fileglob);
+				}
+				catch (Exception ex)
+				{
+					//errors.Add(ex.Message);
+				}
+				if (filelist != null)
+				{
+					foreach (string fn in filelist)
+					{
+						sourcePaths.Add(fn);
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Find the directories in the given source folder and then scan for the files in each of those directories.
+		/// Calls ProcessSourcePaths as the final step to populate the destPaths list.
+		/// </summary>
+		private void ScanFolder()
+		{
+			listBoxSource.DataSource = null;
+			sourcePaths.Clear();
+			listBoxFinal.DataSource = null;
+			destPaths.Clear();
+			string[] exts = textExtensions.Text.Split(';');
+			try
+			{
+				//Queue<String> folderpaths = new Queue<string>();
+				//while (folderpaths.Count > 0)
+				{
+					string folderpath = textFolder.Text;
+					IEnumerable<string> folderlist = null;
+					try
+					{
+						folderlist = Directory.EnumerateDirectories(folderpath, "*");//* for folders so we can search those
+					}
+					catch (Exception ex)
+					{
+						//errors.Add(ex.Message);
+					}
+					if (folderlist != null)
+					{
+						foreach (string fp in folderlist)
+						{
+							//folderpaths.Enqueue(fp);
+							ScanForFiles(fp, exts);
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				sourcePaths.Add(ex.Message);
+			}
+			listBoxSource.DataSource = sourcePaths;
+			ProcessSourcePaths();
+		}
+
+		/// <summary>
+		/// Transform the list in sourcePaths to what the new names will be and place in destPaths
+		/// </summary>
+		void ProcessSourcePaths()
+		{
+			listBoxFinal.DataSource = null;
+			destPaths.Clear();
+			bool bParent = checkSetNames.Checked;
+			foreach (string src in sourcePaths)
+			{
+				string subPath = Path.GetDirectoryName(src);
+				string fullPath = Path.GetFullPath(src);
+				string ext = Path.GetExtension(src);
+				string basePath = Path.GetDirectoryName(subPath);
+				string filename = Path.GetFileNameWithoutExtension(src);
+				if (bParent)
+				{
+					filename = Path.GetFileName(subPath);
+				}
+				string destPath = Path.Combine(basePath, filename) + ext;
+				destPaths.Add(destPath);
+			}
+			listBoxFinal.DataSource = destPaths;
+
+			if (destPaths.Count > 0 && sourcePaths.Count > 0)
+			{
+				buttonRename.Enabled = true;
+			}
+		}
+
+		private void OnClosing(object sender, FormClosingEventArgs e)
+		{
+			if (textFolder.Text.Count() > 0 || textExtensions.Text.Count() > 0)
+			{
+				RegistryKey rk = null;
+				rk = Registry.CurrentUser;
+				RegistryKey key = rk.CreateSubKey(SVRregPath);
+				if (key != null)
+				{
+					try
+					{
+						key.SetValue("LastFolder", textFolder.Text);
+						key.SetValue("Extensions", textExtensions.Text);
+						key.SetValue("Set", checkSetNames.Checked, RegistryValueKind.DWord);
+					}
+					catch (Exception ex)
+					{
+						//errors.Add(ex.Message);
+					}
+					finally
+					{
+						key.Close();
+					}
+				}
+			}
+		}
+
+		private void OnFolderChanged(object sender, EventArgs e)
+		{
+			if (textFolder.Text.Count() > 0 && textExtensions.Text.Count() > 0)
+			{
+				ScanFolder();
+			}
+		}
+
+		private void OnExtensionsChanged(object sender, EventArgs e)
+		{
+			if (textFolder.Text.Count() > 0 && textExtensions.Text.Count() > 0)
+			{
+				ScanFolder();
+			}
+		}
+
+		private void OnClickedRename(object sender, EventArgs e)
+		{
+			buttonRename.Enabled = false;
+
+			int nCnt = destPaths.Count;
+			for (int i = 0; i < nCnt; i++)
+			{
+				try
+				{
+					File.Move(sourcePaths[i], destPaths[i]);
+				}
+				catch (Exception ex)
+				{
+					sourcePaths[i] = ex.Message;
+				}
+			}
+		}
+
+		private void OnClickedBrowse(object sender, EventArgs e)
+		{
+			FolderBrowserDialog fldr = new FolderBrowserDialog();
+			fldr.Description = "Select the base folder for the scan.";
+			fldr.ShowNewFolderButton = false;
+			if (textFolder.Text.Length > 0)
+				fldr.SelectedPath = textFolder.Text;
+
+			if (fldr.ShowDialog() == DialogResult.OK)
+			{
+				textFolder.Text = fldr.SelectedPath;
+			}
+		}
+
+		private void OnCheckedSetNames(object sender, EventArgs e)
+		{
+			if (textFolder.Text.Count() > 0 && textExtensions.Text.Count() > 0)
+			{
+				if (sourcePaths.Count < 1)
+					ScanFolder();
+				else
+					ProcessSourcePaths();
+			}
+		}
+	}
+}
